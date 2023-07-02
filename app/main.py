6586ed98a4842934300af6d3347e22a26987427d
Lic. Emilio Costich <ecostichp@gmail.com>
2023-07-01 17:24:05 -0700

    Reinié base de datos

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    # "http://192.168.1.71:5173",
 ]
 
 app.add_middleware(
