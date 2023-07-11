9db65da9bf08c2944157e4ff75b4976f8f9c8c37
Lic. Emilio Costich <ecostichp@gmail.com>
2023-07-11 15:33:07 -0700

    Intento de conexción por Cloud SQL Python Connector 01

--- app/orm.py
+++ app/orm.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from google.cloud.sql.connector import Connector, IPTypes
 
 from .config import settings
 
@@ -8,6 +10,34 @@ from .config import settings
 # DATABASE_URL = "sqlite:///app/database.db"
 # engine = create_engine( DATABASE_URL, connect_args={"check_same_thread": False} )
 
+
+
+# helper function to return SQLAlchemy connection pool
+def init_connection_pool(connector: Connector) -> Engine:
+    # Python Connector database connection function
+    def getconn():
+        conn = connector.connect(
+            settings.gcloud_sql_name, # Cloud SQL Instance Connection Name
+            "pg8000",
+            user= settings.gcloud_sql_user,
+            password= settings.gcloud_sql_password,
+            db= settings.gcloud_sql_db,
+            ip_type= IPTypes.PUBLIC  # IPTypes.PRIVATE for private IP
+        )
+        return conn
+
+    SQLALCHEMY_DATABASE_URL = "postgresql+pg8000://"
+
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL , creator=getconn
+    )
+    return engine
+
+# initialize Cloud SQL Python Connector
+connector = Connector()
+
+# create connection pool engine
+engine = init_connection_pool(connector)
 
 
 
