eeccc70be4c9d4e1a8baa20775ea39902b5d10fe
Lic. Emilio Costich <ecostichp@gmail.com>
2023-10-17 15:54:26 -0700

    a

--- gcsfuse_run.sh
+++ gcsfuse_run.sh
@@ -8,6 +8,7 @@ echo "Mounting Google Cloud Store Fuse."
 gcsfuse --debug_gcs --debug_fuse $BUCKET_NAME $MNT_DIR
 echo "Mounting completed."
 
+exec ls
 
 # Run the web service on container startup. Here we use the gunicorn
 exec uvicorn app.main:app --host 0.0.0.0 --port 8080
