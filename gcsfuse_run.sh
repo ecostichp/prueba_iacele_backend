1a0fbcec10c3878749d6b5d69d2ede765fbcc320
Lic. Emilio Costich <ecostichp@gmail.com>
2023-10-17 16:30:15 -0700

    05 Intento de variable en docker

--- gcsfuse_run.sh
+++ gcsfuse_run.sh
@@ -2,13 +2,14 @@
 set -eo pipefail
 
 # Create mount directory for service
+echo "La variable es :" $MNT_DIR
 mkdir -p $MNT_DIR
 
 echo "Mounting Google Cloud Store Fuse."
 gcsfuse --debug_gcs --debug_fuse $BUCKET_NAME $MNT_DIR
 echo "Mounting completed."
 
-exec ls
+ls
 
 # Run the web service on container startup. Here we use the gunicorn
 exec uvicorn app.main:app --host 0.0.0.0 --port 8080
